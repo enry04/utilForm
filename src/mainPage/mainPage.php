312e614a466d3fced8e688ff3c8da5de43049247
enry04 <enrico04v@gmail.com>
2022-11-18 13:57:01 +0100

    (#7) added animation, init form

--- src/mainPage/mainPage.php
+++ src/mainPage/mainPage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,41 +10,53 @@
     <link rel="stylesheet" href="../common/css/pageStyle.css">
 </head>
 <body>
-    <header>
-        <div class="header-container">
-            <div class="title-container">
-                <h1 class="title-text">backoffice exercise</h1>
-            </div>
-        </div>
-    </header>
     <main>
-        <div class="form-container">
-            <h2>form</h2>
-            <form method="post">
-                <div class="items-container">
-                    <div class="item">
-                        <input type="text" placeholder="some text" name="textBox">
-                        <label for="textBox">text</label>
-                    </div>
-                    <div class="item">
-                        <input type="number" placeholder="some numbers" name="numberBox">
-                        <label for="numberBox">number</label>
-                    </div>
+        <div class="main-container">
+            <div class="form-container">
+                <div class="top-container">
+                    <h2>Form</h2>
                 </div>
-                <div class="items-container">
-                    <div class="item">
-                        <input type="date" name="dateBox">
-                        <label for="dateBox">date</label>
-                        <select>
-                            <option value="">default</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                        </select>
-                    </div>
+                <div class="inputs-container">
+                    <form method="post">
+                        <div class="items-container">
+                            <div class="item">
+                                <input type="text" placeholder="some text" name="textBox">
+                                <label for="textBox">text</label>
+                            </div>
+                            <div class="item">
+                                <input type="number" placeholder="some numbers" name="numberBox">
+                                <label for="numberBox">number</label>
+                            </div>
+                        </div>
+                        <!-- <div class="items-container">
+                            <div class="item">
+                                <input type="date" name="dateBox">
+                                <label for="dateBox">date</label>
+                                <select>
+                                    <option value="">default</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="radio-check-container">
+                                <input type="file" name="fileBox" id="fileBox">
+                                <label for="fileBox">file</label>
+                                <input type="radio" name="radio" value="1">
+                                <label for="1">1</label>
+                                <input type="radio" name="radio" value="2">
+                                <label for="2">2</label>
+                                <input type="radio" name="radio" value="3">
+                                <label for="3">3</label>
+                                <input type="radio" name="radio" value="4">
+                                <label for="4">4</label>
+                            </div> -->
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </main>
 </body>
+
 </html>
