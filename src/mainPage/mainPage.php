4378b0ed691fde767ea18d70bc83a3ae5d194609
enry04 <enrico04v@gmail.com>
2022-11-20 11:52:44 +0100

    (#7) finished first row

--- src/mainPage/mainPage.php
+++ src/mainPage/mainPage.php
@@ -30,22 +30,28 @@
                             </div>
                         </div>
                         <div class="second-row-container row">
-                            <input type="date">
-                            <span>date</span>
-                            <input type="file">
-                            <span>file</span>
-                            <select>
-                                <option value="1">1</option>
-                                <option value="1">1</option>
-                                <option value="1">1</option>
-                                <option value="1">1</option>
-                            </select>
+                            <div class="input-container">
+                                <input type="date" required="required">
+                                <span>date</span>
+                            </div>
+                            <div class="input-container">
+                                <input type="file">
+                                <span>file</span>
+                            </div>
+                            <div class="input-container">
+                                <select>
+                                    <option value="1">1</option>
+                                    <option value="1">1</option>
+                                    <option value="1">1</option>
+                                    <option value="1">1</option>
+                                </select>
+                            </div>
                         </div>
                         <div class="third-row-container row">
-                            <input type="radio">
-                            <input type="radio">
-                            <input type="radio">
-                            <input type="radio">
+                            <input type="radio" name="radio" required="required">
+                            <input type="radio" name="radio" required="required">
+                            <input type="radio" name="radio" required="required">
+                            <input type="radio" name="radio" required="required">
                             <input type="checkbox">
                         </div>
                         <div class="fourth-row-container row">
@@ -53,40 +59,6 @@
                         </div>
                     </form>
                 </div>
-                <!-- <div class="inputs-container">
-                    <form method="post">
-                        <div class="items-container">
-                            <div class="item">
-                                <input type="text" placeholder="text" name="textBox">
-                            </div>
-                            <div class="item">
-                                <input type="number" placeholder="number" name="numberBox">
-                            </div>
-                        </div> -->
-                <!-- <div class="items-container">
-                            <div class="item">
-                                <input type="date" name="dateBox">
-                                <label for="dateBox">date</label>
-                                <select>
-                                    <option value="">default</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="radio-check-container">
-                                <input type="file" name="fileBox" id="fileBox">
-                                <label for="fileBox">file</label>
-                                <input type="radio" name="radio" value="1">
-                                <label for="1">1</label>
-                                <input type="radio" name="radio" value="2">
-                                <label for="2">2</label>
-                                <input type="radio" name="radio" value="3">
-                                <label for="3">3</label>
-                                <input type="radio" name="radio" value="4">
-                                <label for="4">4</label>
-                            </div> -->
                 </form>
             </div>
         </div>
