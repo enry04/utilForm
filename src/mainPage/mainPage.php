2f72460da6fa8e94082eac5680fc4e70563b827b
enry04 <enrico04v@gmail.com>
2022-11-27 22:01:21 +0100

    (#25) init table

--- src/mainPage/mainPage.php
+++ src/mainPage/mainPage.php
@@ -23,6 +23,53 @@
             <input type="button" value="Aggiungi record">
         </a>
     </div>
+
+    <main>
+        <div class="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>testo</th>
+                        <th>data</th>
+                        <th>camposn</th>
+                        <th>numero</th>
+                        <th>percorso</th>
+                        <th>idSupporto</th>
+                        <th>idRadioet</th>
+                        <th>modifica</th>
+                        <th>elimina</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>ssdsd</td>
+                        <td>323/32/3232</td>
+                        <td>1</td>
+                        <td>434343</td>
+                        <td>C:/djnd/dshdb/djsdn</td>
+                        <td>1</td>
+                        <td>4</td>
+                        <td>modifica</td>
+                        <td>elimina</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>ssdsd</td>
+                        <td>323/32/3232</td>
+                        <td>1</td>
+                        <td>34344</td>
+                        <td>C:/djnd/dshdb/djsdn</td>
+                        <td>1</td>
+                        <td>4</td>
+                        <td>modifica</td>
+                        <td>elimina</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
 </body>
 
 </html>
